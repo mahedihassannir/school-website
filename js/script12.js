54e2981fd87cc862d8c253ab56c707dec51773e8
mahedi6666655555 <mahedinir34678@gmail.com>
2023-02-13 14:15:33 +0600

    done

---
+++ js/script12.js
@@ -0,0 +1,29 @@
+document.getElementById("btn-submit").addEventListener("click", function () {
+
+let input1=document.getElementById("mail")
+let forvalue=input1.value
+
+
+let document2=document.getElementById("pass")
+
+let forvalue2=document2.value
+
+
+if(forvalue==="mahedi"&&forvalue2==="mahedi"){
+    // window.location.href="index.html"
+    window.location.href="video.html"
+}
+else{
+
+alert("not right value")
+
+}
+
+
+
+
+
+
+
+
+});
